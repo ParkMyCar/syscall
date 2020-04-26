5df2c493d57b6d8ac8bf999995971bc50bb2d693
Parker Timmerman <parker@parkertimmerman.com>
2020-04-26 15:53:25 -0400

    removing libc dep for syscall-num

--- syscall-rs/src/lib.rs
+++ syscall-rs/src/lib.rs
@@ -1,7 +1,9 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+use syscall_macro::syscall;
+
+pub use platform::*;
+
+#[cfg(target_os = "macos")]
+#[path = "platform/macos.rs"]
+pub mod platform;
+
+pub mod macros;
