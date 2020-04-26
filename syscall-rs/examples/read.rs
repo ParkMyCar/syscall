5217a1fc2d78d3c37e43105e52191536f72e1991
Parker Timmerman <parker@parkertimmerman.com>
2020-04-26 16:12:55 -0400

    adding read example

---
+++ syscall-rs/examples/read.rs
@@ -0,0 +1,16 @@
+use std::fs::File;
+
+use bytes::BytesMut;
+use syscall_rs::read;
+
+fn main() -> Result<(), std::io::Error> {
+    let mut buf = BytesMut::with_capacity(16);
+    buf.resize(16, 0); // with_capacity(...) does not allocate
+
+    let file = File::open("examples/files/hello_world.txt")?;
+    
+    read(file, &mut buf, 16)?;
+    println!("{:#?}", buf);
+
+    Ok(())
+}
--- syscall-rs/src/platform/macos.rs
+++ syscall-rs/src/platform/macos.rs
@@ -1,10 +1,23 @@
 use crate::parse_os_error;
 
-use std::convert::AsRef;
+use std::convert::{AsMut, AsRef};
 use std::os::unix::io::AsRawFd;
 
 use syscall_macro::syscall;
 use syscall_num::*;
+
+pub fn read<F, B>(fd: F, mut buf: B, num: usize) -> std::io::Result<usize>
+where 
+    F: AsRawFd,
+    B: AsMut<[u8]>,
+{
+    let raw_fd = fd.as_raw_fd();
+    let buf_ref = buf.as_mut();
+
+    parse_os_error!(
+        syscall!(READ, raw_fd, buf_ref.as_mut_ptr(), num)
+    )
+}
 
 pub fn write<F, B>(fd: F, bytes: B) -> std::io::Result<usize>
 where
